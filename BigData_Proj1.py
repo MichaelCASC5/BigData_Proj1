50395a82ad1232087ccc098bd266061da141e9f1
meromeofbacca <60234156+meromeofbacca@users.noreply.github.com>
2022-10-11 12:53:03 -0400

    Add files via upload

--- BigData_Proj1.py
+++ BigData_Proj1.py
@@ -6,7 +6,11 @@ Created on Tue Oct  4 15:24:39 2022
 """
 from py2neo import Graph, Node, Relationship, cypher
 
-graph = Graph("bolt://localhost:7687", auth=("neo4j", "12345678"))
-graph.run("LOAD CSV FROM ‘C:\\Users\merom\Desktop\Big Data\nodes.tsv' AS line \
-          FIELDTERMINATOR ‘\t’ CREATE (:Node { id: line[1], name: line[2], type: line[3]})")
+graph = Graph("bolt://localhost:11003", auth=("neo4j", "12345678"))
+query = """
+LOAD CSV FROM "file:///C:/Users/merom/Desktop/Big_Data/nodes.tsv" AS line FIELDTERMINATOR "\t"
+CREATE (:Node { id: line[1], name: line[2], type: line[3]})
+"""
 
+#graph.update()
+print(graph.run(query).stats())
