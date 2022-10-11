4984c4d79fadec9039cdd53916e3b6803e4010e0
meromeofbacca <60234156+meromeofbacca@users.noreply.github.com>
2022-10-11 12:54:09 -0400

    Add files via upload

--- BigData_Proj1.py
+++ BigData_Proj1.py
@@ -6,11 +6,11 @@ Created on Tue Oct  4 15:24:39 2022
 """
 from py2neo import Graph, Node, Relationship, cypher
 
+#creates all nodes with unique label, need to edit to have nodes with correct label
 graph = Graph("bolt://localhost:11003", auth=("neo4j", "12345678"))
 query = """
 LOAD CSV FROM "file:///C:/Users/merom/Desktop/Big_Data/nodes.tsv" AS line FIELDTERMINATOR "\t"
 CREATE (:Node { id: line[1], name: line[2], type: line[3]})
 """
 
-#graph.update()
 print(graph.run(query).stats())
