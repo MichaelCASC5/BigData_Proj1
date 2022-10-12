357faafdae8c70ea46fb19f8b5ecca1e159ace03
meromeofbacca <60234156+meromeofbacca@users.noreply.github.com>
2022-10-11 23:14:48 -0400

    Add files via upload
    Added relationship queries, database now fully created with nodes and relationships

--- BigData_Proj1.py
+++ BigData_Proj1.py
@@ -5,49 +5,75 @@ Created on Tue Oct  4 15:24:39 2022
 @author: Viet Dinh
 @author: Michael Calle
 """
+#Precursors on Neo4j db settings found in Neo4j Desktop
+#Must comment out dbms.directories.import=import else can't read tsv files in other dirs
+#Set dbms.memory.heap.max_size=3G (default 1G) to improve relationship creation speed
+
+
 from py2neo import Graph, Node, Relationship, cypher
 
-#Nodes have labels of [Disease, Gene, Compound, Anatomy]
-#Nodes have columns of id Ex:Compound::DB00661 and name Ex:Verapamil
+#Connects to Neo4j database
+graph = Graph("bolt://localhost:11006", auth=("neo4j", "12345678"))
 
-graph = Graph("bolt://localhost:11003", auth=("neo4j", "12345678"))
-
-query = """
+#Creates nodes with label "Node" and property "type"
+nquery = """
 LOAD CSV FROM "file:///C:/Users/merom/Desktop/Big_Data/nodes.tsv" AS line FIELDTERMINATOR "\t"
 CREATE (:Node { id: line[0], name: line[1], type: line[2]})
 """
-#These queries assign nodes with correct labels
-query2 = """
+
+#Queries assign nodes with correct labels and removes property "type"
+nquery2 = """
 MATCH (n:Node {type:'Anatomy'})
 SET n:Anatomy
 REMOVE n:Node
 REMOVE n.type;
 """
-
-query3 = """
+nquery3 = """
 MATCH (n:Node {type:'Gene'})
 SET n:Gene
 REMOVE n:Node
 REMOVE n.type;
 """
-
-query4 = """
+nquery4 = """
 MATCH (n:Node {type:'Disease'})
 SET n:Disease
 REMOVE n:Node
 REMOVE n.type;
 """
-
-query5 = """ 
+nquery5 = """ 
 MATCH (n:Node {type:'Compound'})
 SET n:Compound
 REMOVE n:Node
 REMOVE n.type;
 """
 
-print(graph.run(query).stats())
-print(graph.run(query2).stats())
-print(graph.run(query3).stats())
-print(graph.run(query4).stats())
-print(graph.run(query5).stats())
+#Creates relationships with metaedge as property "metaedge"
+rquery = """
+LOAD CSV WITH HEADERS FROM "file:///C:/Users/merom/Desktop/Big_Data/edges.tsv" AS row 
+FIELDTERMINATOR "\t"
+
+MATCH (n1:Node {id: row.source})
+MATCH (n2:Node {id: row.target})
+MERGE (n1)-[:Relates {metaedge: row.metaedge}]->(n2)
+"""
+
+#Adding constraint MASSIVELY improves relationship creation time
+constraintQuery = """
+CREATE CONSTRAINT id ON (n:Node) ASSERT n.id IS UNIQUE
+"""
+
+#Queries only need to be ran once at start to create database; comment out after
+print(graph.run(constraintQuery).stats())
+print(graph.run(rquery).stats())
+print(graph.run(nquery).stats())
+print(graph.run(nquery2).stats())
+print(graph.run(nquery3).stats())
+print(graph.run(nquery4).stats())
+print(graph.run(nquery5).stats())
+
+
+#Queries for deletion:
+#print(graph.run("MATCH (n) DETACH DELETE n").stats())
+#print(graph.run("DROP CONSTRAINT ON (n:Node) ASSERT n.id IS UNIQUE").stats())
+
 
