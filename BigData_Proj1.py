7a117b5ac3be66e462f718d3e0f3509ee41a0059
meromeofbacca <60234156+meromeofbacca@users.noreply.github.com>
2022-10-13 03:51:19 -0400

    Created command line interface framework

--- BigData_Proj1.py
+++ BigData_Proj1.py
@@ -10,70 +10,98 @@ Created on Tue Oct  4 15:24:39 2022
 #Set dbms.memory.heap.max_size=3G (default 1G) to improve relationship creation speed
 
 
-from py2neo import Graph, Node, Relationship, cypher
+from py2neo import Graph
+import typer
 
+app = typer.Typer()
 #Connects to Neo4j database
 graph = Graph("bolt://localhost:11006", auth=("neo4j", "12345678"))
 
-#Creates nodes with label "Node" and property "type"
-nquery = """
-LOAD CSV FROM "file:///C:/Users/merom/Desktop/Big_Data/nodes.tsv" AS line FIELDTERMINATOR "\t"
-CREATE (:Node { id: line[0], name: line[1], type: line[2]})
-"""
+@app.command()
+def createneodb():
+    #Creates nodes with label "Node" and property "type"
+    nquery = """
+    LOAD CSV FROM "file:///C:/Users/merom/Desktop/Big_Data/nodes.tsv" AS line FIELDTERMINATOR "\t"
+    CREATE (:Node { id: line[0], name: line[1], type: line[2]})
+    """
+    
+    #Queries assign nodes with correct labels and removes property "type"
+    nquery2 = """
+    MATCH (n:Node {type:'Anatomy'})
+    SET n:Anatomy
+    REMOVE n:Node
+    REMOVE n.type;
+    """
+    nquery3 = """
+    MATCH (n:Node {type:'Gene'})
+    SET n:Gene
+    REMOVE n:Node
+    REMOVE n.type;
+    """
+    nquery4 = """
+    MATCH (n:Node {type:'Disease'})
+    SET n:Disease
+    REMOVE n:Node
+    REMOVE n.type;
+    """
+    nquery5 = """ 
+    MATCH (n:Node {type:'Compound'})
+    SET n:Compound
+    REMOVE n:Node
+    REMOVE n.type;
+    """
+    
+    #Creates relationships with metaedge as property "metaedge"
+    rquery = """
+    LOAD CSV WITH HEADERS FROM "file:///C:/Users/merom/Desktop/Big_Data/edges.tsv" AS row 
+    FIELDTERMINATOR "\t"
+    
+    MATCH (n1:Node {id: row.source})
+    MATCH (n2:Node {id: row.target})
+    MERGE (n1)-[:Relates {metaedge: row.metaedge}]->(n2)
+    """
+    
+    #Adding constraint MASSIVELY improves relationship creation time
+    constraintQuery = """
+    CREATE CONSTRAINT id ON (n:Node) ASSERT n.id IS UNIQUE
+    """
+    
+    #Queries only need to be ran once at start to create database; comment out after
+    print(graph.run(nquery).stats())
+    print(graph.run(constraintQuery).stats())
+    print(graph.run(rquery).stats())
+    print(graph.run(nquery2).stats())
+    print(graph.run(nquery3).stats())
+    print(graph.run(nquery4).stats())
+    print(graph.run(nquery5).stats())
+    
+    
+@app.command()
+def deleteneodb():
+    print(graph.run("DROP CONSTRAINT ON (n:Node) ASSERT n.id IS UNIQUE").stats())
+    print(graph.run("MATCH (n) DETACH DELETE n").stats())
+    
+@app.command()
+def neodiseaseinfo():
+    #NEED TO DO
+    print(graph.run("""MATCH (d:Disease) WHERE d.id = 'Disease::DOID:7148' MATCH 
+                       (c1:Compound)-[r1:Relates]->(d) WHERE r1.metaedge='CpD' MATCH 
+                       (c2:Compound)-[r2:Relates]->(d) WHERE r2.metaedge='CtD' MATCH 
+                       (d)-[r3:Relates]->(g:Gene) WHERE r3.metaedge='DaG' MATCH 
+                       (d)-[r4:Relates]->(a:Anatomy) WHERE r4.metaedge='DlA' 
+                       RETURN d.name,c1.name,c2.name,g.name,a.name"""))
 
-#Queries assign nodes with correct labels and removes property "type"
-nquery2 = """
-MATCH (n:Node {type:'Anatomy'})
-SET n:Anatomy
-REMOVE n:Node
-REMOVE n.type;
-"""
-nquery3 = """
-MATCH (n:Node {type:'Gene'})
-SET n:Gene
-REMOVE n:Node
-REMOVE n.type;
-"""
-nquery4 = """
-MATCH (n:Node {type:'Disease'})
-SET n:Disease
-REMOVE n:Node
-REMOVE n.type;
-"""
-nquery5 = """ 
-MATCH (n:Node {type:'Compound'})
-SET n:Compound
-REMOVE n:Node
-REMOVE n.type;
-"""
+#@app.command()
+#def neocmpdtreatdisease():
+    #ADD QUERY
+    
+#REFERENCE
+#@app.command()
+#def hello(name:str, iq:int, display_iq:bool = True):
+#    print(f"hello {name}")
+#    if(display_iq):
+#        print(f"your iq is: {iq}")
+    
+if __name__ == "__main__":
+    app()
 
-#Creates relationships with metaedge as property "metaedge"
-rquery = """
-LOAD CSV WITH HEADERS FROM "file:///C:/Users/merom/Desktop/Big_Data/edges.tsv" AS row 
-FIELDTERMINATOR "\t"
-
-MATCH (n1:Node {id: row.source})
-MATCH (n2:Node {id: row.target})
-MERGE (n1)-[:Relates {metaedge: row.metaedge}]->(n2)
-"""
-
-#Adding constraint MASSIVELY improves relationship creation time
-constraintQuery = """
-CREATE CONSTRAINT id ON (n:Node) ASSERT n.id IS UNIQUE
-"""
-
-#Queries only need to be ran once at start to create database; comment out after
-print(graph.run(nquery).stats())
-print(graph.run(constraintQuery).stats())
-print(graph.run(rquery).stats())
-print(graph.run(nquery2).stats())
-print(graph.run(nquery3).stats())
-print(graph.run(nquery4).stats())
-print(graph.run(nquery5).stats())
-
-
-#Queries for deletion:
-#print(graph.run("MATCH (n) DETACH DELETE n").stats())
-#print(graph.run("DROP CONSTRAINT ON (n:Node) ASSERT n.id IS UNIQUE").stats())
-
-#graph.run("MATCH (n:Gene)-[r:Relates]->(n2:Gene) WHERE n.id = 'Gene::125150' OR n2.id ='Gene::125150' RETURN n.id,r,n2.id")
